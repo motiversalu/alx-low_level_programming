f8027d1e9398491e03dbef1d4d92df5a3e65b0f7
motiversalu <brainstormbright@gmail.com>
2023-03-17 04:02:09 -0700

    task 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -17,4 +17,4 @@ int main(void)
 	printf("Size of a long long int: %lu byte(S)\n", (unsigned long)sizeof(d));
 	printf("Size of a float: %lu byte(S)\n", (unsigned long)sizeof(f));
 	return (0);
-i}
+}
