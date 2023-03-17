22ecd01079c4c74d18591181bb4c6225bb7809bf
motiversalu <brainstormbright@gmail.com>
2023-03-17 05:03:19 -0700

    task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,7 +5,7 @@
  */
 int main(void)
 {
-    char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+    char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
     write(2, message, 60);
     return 1;
 }
