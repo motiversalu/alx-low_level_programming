a5dcab25463e5de15dcdf6338df7f8d644f6d6a2
motiversalu <brainstormbright@gmail.com>
2023-03-26 09:14:11 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,11 @@
 #include <unistd.h>
+#include <unistd.h>
 /**
  * main - prints a piece of text without using printf or puts
  * Return: 1
  */
 int main(void)
 {
-	char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-	write(2, message, 59);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",59);
 	return (1);
 }
